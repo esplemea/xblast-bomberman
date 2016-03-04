6fd1ede168efd8ee8121cf2d265deaad762829f9
Nicolas Zimmermann <nicolas.zimmermann@epfl.ch>
2016-03-04 13:30:48 +0100

    étape 2

--- src/ch/epfl/xblast/Board.java
+++ src/ch/epfl/xblast/Board.java
@@ -7,131 +7,140 @@ import java.util.List;
 import ch.epfl.cs108.Sq;
 
 public final class Board {
-    
+
 	private final List<Sq<Block>> blocks;
-	
+
 	/**
 	 * Construct a Board from the blocks sequences
 	 * 
 	 * @param blocks
-	 *             List of the Board's sequences
-	 * @throws IllegalArgumentException 
+	 *            List of the Board's sequences
+	 * @throws IllegalArgumentException
 	 *             whether the list does not contain 195 sequences
 	 */
-	
-	public Board(List<Sq<Block>> blocks) throws IllegalArgumentException
-	{
-	    if(blocks.size()!=195)
-	    {
-	        throw new IllegalArgumentException();
-	    }
-	    else
-	    {
-	        ArrayList<Sq<Block>> liste = new ArrayList<>(blocks);
-	        this.blocks = Collections.unmodifiableList(liste);
-	    }
+	public Board(List<Sq<Block>> blocks) {
+		if (blocks.size() != Cell.COLUMNS * Cell.ROWS) {
+			throw new IllegalArgumentException("The list must contain exactly 195 sequences");
+		}
+		ArrayList<Sq<Block>> liste = new ArrayList<>(blocks);
+		this.blocks = Collections.unmodifiableList(liste);
+
 	}
-	
+
 	/**
 	 * Construct a board based on the given matrix
 	 * 
 	 * @param rows
-	 *           the matrix of Block
+	 *            the matrix of Block
 	 * @throws IllegalArgumentException
-	 *           if the matrix isn't 13x15
+	 *             if the matrix isn't 13x15
 	 * @return the board
 	 */
-	
-	public static Board ofRows(List<List<Block>> rows)
-	{
-	    checkBlockMatrix(rows,Cell.ROWS,Cell.COLUMNS);
-	    
-	    List<Sq<Block>> a = new ArrayList<Sq<Block>>();
-	    for (int i = 0; i < Cell.ROWS; i++) {
-            for (int j = 0; j < Cell.COLUMNS; j++) {
-                a.add(Sq.constant(rows.get(i).get(j)));
-            }
-        }
-	    
-	    return new Board(a);
+
+	public static Board ofRows(List<List<Block>> rows) {
+		checkBlockMatrix(rows, Cell.ROWS, Cell.COLUMNS);
+
+		List<Sq<Block>> board = new ArrayList<Sq<Block>>();
+		
+		for (List<Block> list : rows)
+			for (Block block : list)
+				board.add(Sq.constant(block));
+
+		return new Board(board);
 	}
-	
+
 	/**
 	 * Construct a walled board with the matrix of the inner Blocks
 	 * 
 	 * @param innerBlocks
-	 *         the matrix of the inner Blocks
+	 *            the matrix of the inner Blocks
 	 * @throws IllegalArgumentException
-     *           if the matrix isn't 11x13
+	 *             if the matrix isn't 11x13
 	 * @return the board
 	 */
-	
-	public static Board ofInnerBlocksWalled(List<List<Block>> innerBlocks)
-	{
-	    checkBlockMatrix(innerBlocks,Cell.ROWS-2,Cell.COLUMNS-2);
-	    
-	    List<Sq<Block>> a = new ArrayList<Sq<Block>>();
-	    
-	    return null;
+
+	public static Board ofInnerBlocksWalled(List<List<Block>> innerBlocks) {
+		checkBlockMatrix(innerBlocks, Cell.ROWS - 2, Cell.COLUMNS - 2);
+
+		List<List<Block>> board = new ArrayList<>();
+		
+		board.add(Collections.nCopies(Cell.COLUMNS, Block.INDESTRUCTIBLE_WALL));
+		
+		List<Block> centralline;
+		for (List<Block> list : innerBlocks) {
+			centralline = new ArrayList<>();
+			centralline.add(Block.INDESTRUCTIBLE_WALL);
+			centralline.addAll(list);
+			centralline.add(Block.INDESTRUCTIBLE_WALL);
+			board.add(centralline);
+		}
+		board.add(Collections.nCopies(Cell.COLUMNS, Block.INDESTRUCTIBLE_WALL));
+		
+		return ofRows(board);
 	}
-	
+
 	/**
 	 * Construct a walled symmetric board based on the Northwest quadrant
 	 * 
 	 * @param quadrantNWBlocks
-	 *         the matrix of the Northwest quadrant
-     * @throws IllegalArgumentException
-     *           if the matrix isn't 6x7
+	 *            the matrix of the Northwest quadrant
+	 * @throws IllegalArgumentException
+	 *             if the matrix isn't 6x7
 	 * @return the board
 	 */
-	
-	public static Board ofQuadrantNWBlocksWalled(List<List<Block>> quadrantNWBlocks)
-	{
-	    checkBlockMatrix(quadrantNWBlocks,(Cell.ROWS+1)/2,(Cell.COLUMNS+1)/2);
-	    
-	    return null;
+
+	public static Board ofQuadrantNWBlocksWalled(List<List<Block>> quadrantNWBlocks) {
+		checkBlockMatrix(quadrantNWBlocks, Cell.ROWS / 2, Cell.COLUMNS / 2);
+		List<List<Block>> board = new ArrayList<>();
+		List<Block> line;
+		for (List<Block> list : quadrantNWBlocks) {
+			line = new ArrayList<>();
+			line.addAll(Lists.mirrored(list));
+			board.add(line);
+		}
+		System.out.println(board.size());
+		System.out.println(board.get(0).size());
+		return ofInnerBlocksWalled(Lists.mirrored(board));
 	}
-	
+
 	/**
-	 * Check whether the matrix placed in parameter does contain "rows" rows and "columns" columns
+	 * Check whether the matrix placed in parameter does contain "rows" rows and
+	 * "columns" columns
 	 * 
-	 * @param a matrix
-	 * @param number of rows expected
-	 * @param number of columns expected
-	 * @throws IllegalArgumentException if the parameters does not match
+	 * @param a
+	 *            matrix
+	 * @param number
+	 *            of rows expected
+	 * @param number
+	 *            of columns expected
+	 * @throws IllegalArgumentException
+	 *             if the parameters does not match
 	 */
-	
-	private static void checkBlockMatrix(List<List<Block>> matrix, int rows, int columns) 
-	        throws IllegalArgumentException
-	{
-	    if(matrix.size()!=rows || matrix.get(0).size()!= columns)
-	    {
-	        throw new IllegalArgumentException();
-	    }
+
+	private static void checkBlockMatrix(List<List<Block>> matrix, int rows, int columns) {
+		if (matrix.size() != rows || matrix.get(0).size() != columns) {
+			throw new IllegalArgumentException();
+		}
 	}
-	
+
 	/**
 	 * @param c
-	 *     The cell from which to return the sequence of Block
-	 * @return
-	 *     The sequence of Block in the cell c.
+	 *            The cell from which to return the sequence of Block
+	 * @return The sequence of Block in the cell c.
 	 */
-	
-	public Sq<Block> blocksAt(Cell c)
-	{
-	    return blocks.get(c.rowMajorIndex());
+
+	public Sq<Block> blocksAt(Cell c) {
+		return blocks.get(c.rowMajorIndex());
 	}
-	
+
 	/**
 	 * @param c
-	 *     The cell from which to return the Block
-	 * @return
-	 *     The head Block from the cell c.
+	 *            The cell from which to return the Block
+	 * @return The head Block from the cell c.
 	 */
-	
-	public Block blockAt(Cell c)
-	{
-	    return blocks.get(c.rowMajorIndex()).head();
+
+	public Block blockAt(Cell c) {
+		return blocks.get(c.rowMajorIndex()).head();
 	}
-	
+
 }
--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -1,33 +1,24 @@
 package ch.epfl.xblast;
 
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public final class Lists {
-    
-    private Lists(){}
-    
-    public static <T> List<T> mirrored(List<T> l)
-        throws IllegalArgumentException
-    {
-        int size = l.size();
-        
-        if(size==0)
-        {
-            throw new IllegalArgumentException();
-        }
-        else
-        {
-            List<T> subList = new ArrayList<T>(l.subList(0, size-1));
-            Collections.reverse(subList);
-            List<T> output = new ArrayList<T>(l);
-            output.addAll(subList);
-            return output;
-        }
-    }
-        
-    
 
+	private Lists() {
+	}
+
+	public static <T> List<T> mirrored(List<T> l){
+
+		if (l.isEmpty()) {
+			throw new IllegalArgumentException("The list to mirror cannot be empty");
+		}
+		
+		List<T> subList = new ArrayList<T>(l.subList(0, l.size() - 1));
+		Collections.reverse(subList);
+		List<T> output = new ArrayList<T>(l);
+		output.addAll(subList);
+		return output;
+	}
 }
-
