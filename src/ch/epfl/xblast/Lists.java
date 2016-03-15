9cb8ab8b987b9b1844051780abee019e7b17ac95
Clara Di Marco <clara.dimarco@epfl.ch>
2016-03-15 16:42:31 +0100

    Fixed Lists

--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -1,6 +1,7 @@
 package ch.epfl.xblast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,42 +14,43 @@ import java.util.List;
 
 public final class Lists {
 
-	private Lists() {
-	}
+    private Lists() {
+    }
 
-	public static <T> List<T> mirrored(List<T> l){
+    public static <T> List<T> mirrored(List<T> l) {
 
-		if (l.isEmpty()) {
-			throw new IllegalArgumentException("The list to mirror cannot be empty");
-		}
-		
-		List<T> subList = new ArrayList<T>(l.subList(0, l.size() - 1));
-		Collections.reverse(subList);
-		List<T> output = new ArrayList<T>(l);
-		output.addAll(subList);
-		return output;
-	}
-	
-	public static <T> List<List<T>> permutations(List<T> l){
-	    if(l.isEmpty()) {
-            return new ArrayList<List<T>>();
+        if (l.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "The list to mirror cannot be empty");
         }
-	    else
-	    {
-	        List<T> subList = new ArrayList<T>(l.subList(1, l.size()));
-	        List<List<T>> temp = permutations(subList);
-	        List<List<T>> output = new ArrayList<List<T>>();
-	        for(List<T> a : temp)
-	        {
-	            for(int i=0; i < a.size(); ++i)
-	            {
-	                List<T> list = a;
-	                list.add(i, l.get(0));
-	                output.add(list);
-	            }
-	        }
-	    
-	        return output;
-	    }
-	}
+
+        List<T> subList = new ArrayList<T>(l.subList(0, l.size() - 1));
+        Collections.reverse(subList);
+        List<T> output = new ArrayList<T>(l);
+        output.addAll(subList);
+        return output;
+    }
+
+    public static <T> List<List<T>> permutations(List<T> l) {
+        if (l.isEmpty()) {
+            return new ArrayList<List<T>>();
+        } else {
+            List<T> subList = new ArrayList<T>(l.subList(1, l.size()));
+            List<List<T>> temp = permutations(subList);
+            List<List<T>> output = new ArrayList<List<T>>();
+            if (temp.isEmpty())
+                output.add(Arrays.asList(l.get(0)));
+            else {
+                for (List<T> a : temp) {
+                    for (int i = 0; i <= a.size(); ++i) {
+                        List<T> list = new ArrayList<T>(a);
+                        list.add(i, l.get(0));
+                        output.add(list);
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
 }
--- test/xblast/ListsTest.java
+++ test/xblast/ListsTest.java
@@ -9,50 +9,46 @@ import org.junit.Test;
 import ch.epfl.xblast.Lists;
 
 public class ListsTest {
-    
+
     @Test
-    public void mirroredWorksOnNonTrivialList()
-    {
+    public void mirroredWorksOnNonTrivialList() {
         List<String> a = Arrays.asList("k", "a", "y");
         a = Lists.mirrored(a);
-        
+
         List<String> expected = Arrays.asList("k", "a", "y", "a", "k");
-        
+
         assertEquals(expected, a);
     }
-    
+
     @Test
-    public void mirroredWorksOnTrivialList()
-    {
+    public void mirroredWorksOnTrivialList() {
         List<String> a = Arrays.asList("k");
         a = Lists.mirrored(a);
-        
+
         List<String> expected = Arrays.asList("k");
-        
+
         assertEquals(expected, a);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
-    public void mirroredFailsOnEmptyList()
-    {
+    public void mirroredFailsOnEmptyList() {
         List<String> a = Arrays.asList();
         a = Lists.mirrored(a);
     }
-    
+
     @Test
-    public void permutationsWorksOnNonTrivialList()
-    {
-        List<Integer> a = Arrays.asList(1,2,3);
+    public void permutationsWorksOnNonTrivialList() {
+        List<Integer> a = Arrays.asList(1, 2, 3);
         List<List<Integer>> permuted = Lists.permutations(a);
-        
+
         assertEquals(6, permuted.size());
-        
-        assertTrue(permuted.contains(Arrays.asList(1,2,3)));
-        assertTrue(permuted.contains(Arrays.asList(1,3,2)));
-        assertTrue(permuted.contains(Arrays.asList(2,1,3)));
-        assertTrue(permuted.contains(Arrays.asList(2,3,1)));
-        assertTrue(permuted.contains(Arrays.asList(3,1,2)));
-        assertTrue(permuted.contains(Arrays.asList(3,2,1)));
+
+        assertTrue(permuted.contains(Arrays.asList(1, 2, 3)));
+        assertTrue(permuted.contains(Arrays.asList(1, 3, 2)));
+        assertTrue(permuted.contains(Arrays.asList(2, 1, 3)));
+        assertTrue(permuted.contains(Arrays.asList(2, 3, 1)));
+        assertTrue(permuted.contains(Arrays.asList(3, 1, 2)));
+        assertTrue(permuted.contains(Arrays.asList(3, 2, 1)));
     }
 
 }
