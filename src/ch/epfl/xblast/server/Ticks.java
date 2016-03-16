8dfa9bc5a4765923e76ecf2053b54f4885f21442
Clara Di Marco <clara.dimarco@epfl.ch>
2016-03-16 14:33:23 +0100

    Update Ticks

--- src/ch/epfl/xblast/server/Ticks.java
+++ src/ch/epfl/xblast/server/Ticks.java
@@ -1,5 +1,7 @@
 package ch.epfl.xblast.server;
 
+import ch.epfl.xblast.Time;
+
 /**
  * 
  * @author Nicolas ZIMMERMANN Clara DI MARCO
@@ -8,10 +10,14 @@ package ch.epfl.xblast.server;
  */
 
 public interface Ticks {
- public static int PLAYER_DYING_TICKS = 8;
- public static int PLAYER_INVULNERABLE_TICKS = 64;
- public static int BOMB_FUSE_TICKS = 100;
- public static int EXPLOSION_TICKS = 30;
- public static int WALL_CRUMBLING_TICKS = EXPLOSION_TICKS;
- public static int BONUS_DISAPPEARING_TICKS = EXPLOSION_TICKS;
+
+    public static int PLAYER_DYING_TICKS = 8;
+    public static int PLAYER_INVULNERABLE_TICKS = 64;
+    public static int BOMB_FUSE_TICKS = 100;
+    public static int EXPLOSION_TICKS = 30;
+    public static int WALL_CRUMBLING_TICKS = EXPLOSION_TICKS;
+    public static int BONUS_DISAPPEARING_TICKS = EXPLOSION_TICKS;
+    public static int TICKS_PER_SECOND = 20;
+    public static int TICK_NANOSECOND_DURATION = Time.NS_PER_S/TICKS_PER_SECOND;
+    public static int TOTAL_TICKS = TICKS_PER_SECOND*2*Time.S_PER_MIN;
 }
