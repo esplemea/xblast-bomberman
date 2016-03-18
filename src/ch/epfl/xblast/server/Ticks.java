94c827d2346465547a3bb7e6a4aa28b70227d074
Nicolas Zimmermann <nicolas.zimmermann@epfl.ch>
2016-03-18 14:53:31 +0100

    add some "final" in Ticks

--- src/ch/epfl/xblast/server/Ticks.java
+++ src/ch/epfl/xblast/server/Ticks.java
@@ -11,13 +11,13 @@ import ch.epfl.xblast.Time;
 
 public interface Ticks {
 
-    public static int PLAYER_DYING_TICKS = 8;
-    public static int PLAYER_INVULNERABLE_TICKS = 64;
-    public static int BOMB_FUSE_TICKS = 100;
-    public static int EXPLOSION_TICKS = 30;
-    public static int WALL_CRUMBLING_TICKS = EXPLOSION_TICKS;
-    public static int BONUS_DISAPPEARING_TICKS = EXPLOSION_TICKS;
-    public static int TICKS_PER_SECOND = 20;
-    public static int TICK_NANOSECOND_DURATION = Time.NS_PER_S/TICKS_PER_SECOND;
-    public static int TOTAL_TICKS = TICKS_PER_SECOND*2*Time.S_PER_MIN;
+    public final static int PLAYER_DYING_TICKS = 8;
+    public final static int PLAYER_INVULNERABLE_TICKS = 64;
+    public final static int BOMB_FUSE_TICKS = 100;
+    public final static int EXPLOSION_TICKS = 30;
+    public final static int WALL_CRUMBLING_TICKS = EXPLOSION_TICKS;
+    public final static int BONUS_DISAPPEARING_TICKS = EXPLOSION_TICKS;
+    public final static int TICKS_PER_SECOND = 20;
+    public final static int TICK_NANOSECOND_DURATION = Time.NS_PER_S/TICKS_PER_SECOND;
+    public final static int TOTAL_TICKS = TICKS_PER_SECOND*2*Time.S_PER_MIN;
 }
