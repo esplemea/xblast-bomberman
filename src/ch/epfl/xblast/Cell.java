10fc8b9c7a43496f8304de351c0f7ef18fee5fdf
Clara Di Marco <clara.dimarco@epfl.ch>
2016-04-11 14:23:54 +0200

    Redo Spiral order

--- src/ch/epfl/xblast/Cell.java
+++ src/ch/epfl/xblast/Cell.java
@@ -12,141 +12,155 @@ import java.util.Objects;
  *
  */
 public final class Cell {
-	public final static int COLUMNS = 15;
-	public final static int ROWS = 13;
-	public final static int COUNT = ROWS * COLUMNS;
-	public final static List<Cell> ROW_MAJOR_ORDER = Collections.unmodifiableList(rowMajorOrder());
-	public final static List<Cell> SPIRAL_ORDER = Collections.unmodifiableList(rowSpiralOrder());
-	private final int mx, my;
-	
-	/**
-	 * Constructor for a cell. The numbers x and y are normalized
-	 * @param the x position of the Cell in the rows
-	 * @param the y position of the Cell in the columns
-	 */
-	public Cell(int x, int y) {
-		mx = Math.floorMod(x, COLUMNS);
-		my = Math.floorMod(y, ROWS);
-	}
+    public final static int COLUMNS = 15;
+    public final static int ROWS = 13;
+    public final static int COUNT = ROWS * COLUMNS;
+    public final static List<Cell> ROW_MAJOR_ORDER = Collections
+            .unmodifiableList(rowMajorOrder());
+    public final static List<Cell> SPIRAL_ORDER = Collections
+            .unmodifiableList(rowSpiralOrder());
+    private final int mx, my;
 
-	/**
-	 * 
-	 * @return the x normalized position of the cell
-	 */
-	public int x() {
-		return mx;
-	}
+    /**
+     * Constructor for a cell. The numbers x and y are normalized
+     * 
+     * @param the
+     *            x position of the Cell in the rows
+     * @param the
+     *            y position of the Cell in the columns
+     */
+    public Cell(int x, int y) {
+        mx = Math.floorMod(x, COLUMNS);
+        my = Math.floorMod(y, ROWS);
+    }
 
-	/**
-	 * 
-	 * @return the y normalized position of the cell
-	 */
-	public int y() {
-		return my;
-	}
-	
-	/**
-	 * 
-	 * @return the index position of the cell in the reading order
-	 */
-	public int rowMajorIndex() {
-		return COLUMNS * my + mx;
-	}
+    /**
+     * 
+     * @return the x normalized position of the cell
+     */
+    public int x() {
+        return mx;
+    }
 
-	/**
-	 * 
-	 * @return an ArrayList of the cells in the reading order
-	 */
-	private static ArrayList<Cell> rowMajorOrder() {
-		ArrayList<Cell> cellules = new ArrayList<Cell>();
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < COLUMNS; j++) {
-				cellules.add(new Cell(j, i));
-			}
-		}
-		return cellules;
-	}
-	
-	/**
-	 * 
-	 * @return an ArrayList of the cells in the spiral order, starting from the top left corner to the right.
-	 */
-	private static ArrayList<Cell> rowSpiralOrder() {
-		int x = 0, y = 0;
-		ArrayList<Cell> cellules = new ArrayList<Cell>();
-		do {
-			for (int i = x; i < COLUMNS - x; i++) {
-				cellules.add(new Cell(i, y));
-			}
-			for (int j = y + 1; j < ROWS - y; j++) {
-				cellules.add(new Cell(COLUMNS - x - 1, j));
-			}
-			if (x < COLUMNS - x - 1 && y < ROWS - y - 1) {
-				for (int i = COLUMNS - x - 2; i > x; i--) {
-					cellules.add(new Cell(i, ROWS - y - 1));
-				}
-				for (int j = ROWS - y - 1; j > y; j--) {
-					cellules.add(new Cell(x, j));
-				}
-			}
-			x++;
-			y++;
-		} while (x < COLUMNS - x && y < ROWS - y);
-		return cellules;
-	}
+    /**
+     * 
+     * @return the y normalized position of the cell
+     */
+    public int y() {
+        return my;
+    }
 
-	/**
-	 * 
-	 * @param a Direction that
-	 * @return the cell next to the current one in the direction that.
-	 */
-	public Cell neighbor(Direction that) {
-		int y = my;
-		int x = mx;
-		switch (that) {
-		case N:
-			--y;
-			break;
-		case S:
-			++y;
-			break;
-		case E:
-			++x;
-			break;
-		case W:
-			--x;
-			break;
-		default:
-			return null;
-		}
-		return new Cell(x, y);
-	}
+    /**
+     * 
+     * @return the index position of the cell in the reading order
+     */
+    public int rowMajorIndex() {
+        return COLUMNS * my + mx;
+    }
 
-	/**
-	 * @param an Object that
-	 * @return true if the object that is a cell with the same position as the current one.
-	 */
-	public boolean equals(Object that) {
-		if (that.getClass() == this.getClass()) {
-			Cell obj = (Cell) that;
-			return (obj.my == this.my && obj.mx == this.mx);
-		}
-		return false;
-	}
+    /**
+     * 
+     * @return an ArrayList of the cells in the reading order
+     */
+    private static ArrayList<Cell> rowMajorOrder() {
+        ArrayList<Cell> cellules = new ArrayList<Cell>();
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                cellules.add(new Cell(j, i));
+            }
+        }
+        return cellules;
+    }
 
-	/**
-	 * @return a description of the cell on a String : "(x,y)"
-	 */
-	public String toString() {
-		return "(" + this.mx + "," + this.my + ")";
-	}
-	
-	/**
-	 * hash using the rowMajorIndex
-	 */
-	@Override
-	public int hashCode(){
-		return Objects.hash(rowMajorIndex());
-	}
+    /**
+     * 
+     * @return an ArrayList of the cells in the spiral order, starting from the
+     *         top left corner to the right.
+     */
+    private static ArrayList<Cell> rowSpiralOrder() {
+
+        ArrayList<Cell> output = new ArrayList<>();
+
+        ArrayList<Integer> ix = new ArrayList<>();
+        ArrayList<Integer> iy = new ArrayList<>();
+        for (int i = 0; i < ROWS; ++i)
+            ix.add(i);
+        for (int i = 0; i < COLUMNS; ++i)
+            iy.add(i);
+        boolean horizontal = true;
+
+        do {
+            ArrayList<Integer> i1, i2;
+            i1 = horizontal ? ix : iy;
+            i2 = horizontal ? iy : ix;
+            int c2 = i2.remove(0);
+            for (int c1 : i1) {
+                Cell c;
+                c = horizontal ? new Cell(c1, c2) : new Cell(c2, c1);
+                output.add(c);
+            }
+            Collections.reverse(i1);
+            horizontal = !horizontal;
+
+        } while (!ix.isEmpty() && !iy.isEmpty());
+
+        return output;
+    }
+
+    /**
+     * 
+     * @param a
+     *            Direction that
+     * @return the cell next to the current one in the direction that.
+     */
+    public Cell neighbor(Direction that) {
+        int y = my;
+        int x = mx;
+        switch (that) {
+        case N:
+            --y;
+            break;
+        case S:
+            ++y;
+            break;
+        case E:
+            ++x;
+            break;
+        case W:
+            --x;
+            break;
+        default:
+            return null;
+        }
+        return new Cell(x, y);
+    }
+
+    /**
+     * @param an
+     *            Object that
+     * @return true if the object that is a cell with the same position as the
+     *         current one.
+     */
+    public boolean equals(Object that) {
+        if (that.getClass() == this.getClass()) {
+            Cell obj = (Cell) that;
+            return (obj.my == this.my && obj.mx == this.mx);
+        }
+        return false;
+    }
+
+    /**
+     * @return a description of the cell on a String : "(x,y)"
+     */
+    public String toString() {
+        return "(" + this.mx + "," + this.my + ")";
+    }
+
+    /**
+     * hash using the rowMajorIndex
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(rowMajorIndex());
+    }
 }
-
