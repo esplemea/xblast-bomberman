00900852a0d6f7b8aedeaad9f48a3743c6eb4dc0
Nicolas Zimmermann <nicolas.zimmermann@epfl.ch>
2016-05-02 12:55:43 +0200

    step 8 finished

--- src/ch/epfl/xblast/server/GameStateSerializer.java
+++ src/ch/epfl/xblast/server/GameStateSerializer.java
@@ -2,6 +2,11 @@ package ch.epfl.xblast.server;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import ch.epfl.xblast.Cell;
+import ch.epfl.xblast.Direction;
+import ch.epfl.xblast.RunLengthEncoder;
 
 /**
  * 
@@ -16,9 +21,35 @@ public final class GameStateSerializer {
 	 */
 	private GameStateSerializer() {
 	};
-	
-	public static List<Byte> serialize(BoardPainter painter, GameState game){
+
+	/**
+	 * serialize the whole game, in this order : blocks, bombs/blasts, players, ticks.
+	 * @param painter
+	 * @param game
+	 * @return the encoded GameState
+	 */
+	public static List<Byte> serialize(BoardPainter painter, GameState game) {
 		List<Byte> output = new ArrayList<>();
+		for (Cell c : Cell.SPIRAL_ORDER) {
+			output.add(painter.byteForCell(game.board(), c));
+		}
+		Set<Cell> blastedCells = game.blastedCells();
+		for (Cell c : Cell.ROW_MAJOR_ORDER) {
+			if (blastedCells.contains(c)) {
+				output.add(ExplosionPainter.byteForBlast(blastedCells.contains(c.neighbor(Direction.N)),
+						blastedCells.contains(c.neighbor(Direction.E)), blastedCells.contains(c.neighbor(Direction.S)),
+						blastedCells.contains(c.neighbor(Direction.W))));
+			} else if (game.bombedCells().containsKey(c)) {
+				output.add(ExplosionPainter.byteForBomb(game.bombedCells().get(c)));
+			} else {
+				output.add(ExplosionPainter.BYTE_FOR_EMPTY);
+			}
+		}
+		for(Player player : game.players()){
+			output.add(PlayerPainter.byteForPlayer(game.ticks(), player));
+		}
+		output.add((byte)(Math.ceil(game.ticks()/2)));
+		output = RunLengthEncoder.encode(output);
 		return output;
 	}
 }
