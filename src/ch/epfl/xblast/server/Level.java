b1b062d5694eccc6b2e6d514f3f97af5a62b5c8a
Clara Di Marco <clara.dimarco@epfl.ch>
2016-05-02 08:45:26 +0200

    Try Level
    Did what I could. Sorry

--- src/ch/epfl/xblast/server/Level.java
+++ src/ch/epfl/xblast/server/Level.java
@@ -1,5 +1,15 @@
 package ch.epfl.xblast.server;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import ch.epfl.xblast.Cell;
+import ch.epfl.xblast.PlayerID;
+import ch.epfl.xblast.server.Block;
+
 /**
  * 
  * @author Nicolas ZIMMERMANN Clara DI MARCO
@@ -8,9 +18,12 @@ package ch.epfl.xblast.server;
  */
 
 public final class Level {
+    
+
+    
 	private final BoardPainter initialBoard;
 	private final GameState initialGame;
-	public final static Level DEFAULT_LEVEL;
+	public final static Level DEFAULT_LEVEL = defaultLevel();
 
 	/**
 	 * Constructor for a level
@@ -39,5 +52,31 @@ public final class Level {
 	 */
 	public GameState getGameState() {
 		return initialGame;
+	}
+	
+	private static Level defaultLevel() {
+	    
+	    List<List<Block>> defaultInnerBlocks = new ArrayList<>();
+	    
+	    Map<Block,BlockImage> defaultPalette = new HashMap<>();
+	    defaultPalette.put(Block.FREE, BlockImage.IRON_FLOOR);
+	    defaultPalette.put(Block.INDESTRUCTIBLE_WALL, BlockImage.DARK_BLOCK);
+	    defaultPalette.put(Block.DESTRUCTIBLE_WALL, BlockImage.EXTRA);
+	    defaultPalette.put(Block.CRUMBLING_WALL, BlockImage.EXTRA_O);
+	    defaultPalette.put(Block.BONUS_BOMB, BlockImage.BONUS_BOMB);
+	    defaultPalette.put(Block.BONUS_RANGE, BlockImage.BONUS_RANGE);
+	    
+	    Board defaultBoard = Board.ofInnerBlocksWalled(defaultInnerBlocks);
+	    
+	    List<Player> defaultPlayers = new ArrayList<>();
+	    defaultPlayers.add(new Player(PlayerID.PLAYER_1,3,new Cell(1,1),2,3));
+	    defaultPlayers.add(new Player(PlayerID.PLAYER_2,3,new Cell(13,1),2,3));
+	    defaultPlayers.add(new Player(PlayerID.PLAYER_3,3,new Cell(13,11),2,3));
+	    defaultPlayers.add(new Player(PlayerID.PLAYER_4,3,new Cell(1,11),2,3));
+	    
+	    BoardPainter defaultBoardPainter = new BoardPainter(defaultPalette, BlockImage.IRON_FLOOR_S);
+	    GameState defaultGame = new GameState(defaultBoard, defaultPlayers);
+	    
+	    return new Level(defaultBoardPainter,defaultGame);
 	}
 }
