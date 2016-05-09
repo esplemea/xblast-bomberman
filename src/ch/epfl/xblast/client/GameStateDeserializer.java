f100da5c497de7ae7f306ccef61bdffa60c6e720
Clara Di Marco <clara.dimarco@epfl.ch>
2016-05-09 15:01:23 +0200

    Add GameState and GameStateDeserializer

---
+++ src/ch/epfl/xblast/client/GameStateDeserializer.java
@@ -0,0 +1,23 @@
+package ch.epfl.xblast.client;
+
+import java.util.List;
+
+import ch.epfl.xblast.client.GameState;
+import ch.epfl.xblast.server.Board;
+
+public final class GameStateDeserializer {
+    
+    public GameStateDeserializer() { }
+    
+    public static GameState deserializeGameState(List<Byte> serializedGameState) {
+        
+        
+        return null;
+    }
+    
+//    private static Board deserializeBoard(List<Byte> serializedBoard){
+//        return null;
+//    }
+
+
+}
