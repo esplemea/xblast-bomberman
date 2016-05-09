a1cf0ea392ff8ea604113c038c8c5a7d3c4ffaae
Clara Di Marco <clara.dimarco@epfl.ch>
2016-05-09 13:24:03 +0200

    Finish defaultLevel

--- src/ch/epfl/xblast/server/Level.java
+++ src/ch/epfl/xblast/server/Level.java
@@ -18,65 +18,88 @@ import ch.epfl.xblast.server.Block;
  */
 
 public final class Level {
-    
 
-    
-	private final BoardPainter initialBoard;
-	private final GameState initialGame;
-	public final static Level DEFAULT_LEVEL = defaultLevel();
+    private final BoardPainter initialBoard;
+    private final GameState initialGame;
+    public final static Level DEFAULT_LEVEL = defaultLevel();
 
-	/**
-	 * Constructor for a level
-	 * 
-	 * @param initialBoard
-	 * @param initialGame
-	 */
-	public Level(BoardPainter initialBoard, GameState initialGame) {
-		this.initialBoard = initialBoard;
-		this.initialGame = initialGame;
-	}
+    /**
+     * Constructor for a level
+     * 
+     * @param initialBoard
+     * @param initialGame
+     */
+    public Level(BoardPainter initialBoard, GameState initialGame) {
+        this.initialBoard = initialBoard;
+        this.initialGame = initialGame;
+    }
 
-	/**
-	 * Getter for the BoardPainter
-	 * 
-	 * @return
-	 */
-	public BoardPainter getBoardPainter() {
-		return initialBoard;
-	}
+    /**
+     * Getter for the BoardPainter
+     * 
+     * @return
+     */
+    public BoardPainter getBoardPainter() {
+        return initialBoard;
+    }
 
-	/**
-	 * Getter for the GameState
-	 * 
-	 * @return
-	 */
-	public GameState getGameState() {
-		return initialGame;
-	}
-	
-	private static Level defaultLevel() {
-	    
-	    List<List<Block>> defaultInnerBlocks = new ArrayList<>();
-	    
-	    Map<Block,BlockImage> defaultPalette = new HashMap<>();
-	    defaultPalette.put(Block.FREE, BlockImage.IRON_FLOOR);
-	    defaultPalette.put(Block.INDESTRUCTIBLE_WALL, BlockImage.DARK_BLOCK);
-	    defaultPalette.put(Block.DESTRUCTIBLE_WALL, BlockImage.EXTRA);
-	    defaultPalette.put(Block.CRUMBLING_WALL, BlockImage.EXTRA_O);
-	    defaultPalette.put(Block.BONUS_BOMB, BlockImage.BONUS_BOMB);
-	    defaultPalette.put(Block.BONUS_RANGE, BlockImage.BONUS_RANGE);
-	    
-	    Board defaultBoard = Board.ofInnerBlocksWalled(defaultInnerBlocks);
-	    
-	    List<Player> defaultPlayers = new ArrayList<>();
-	    defaultPlayers.add(new Player(PlayerID.PLAYER_1,3,new Cell(1,1),2,3));
-	    defaultPlayers.add(new Player(PlayerID.PLAYER_2,3,new Cell(13,1),2,3));
-	    defaultPlayers.add(new Player(PlayerID.PLAYER_3,3,new Cell(13,11),2,3));
-	    defaultPlayers.add(new Player(PlayerID.PLAYER_4,3,new Cell(1,11),2,3));
-	    
-	    BoardPainter defaultBoardPainter = new BoardPainter(defaultPalette, BlockImage.IRON_FLOOR_S);
-	    GameState defaultGame = new GameState(defaultBoard, defaultPlayers);
-	    
-	    return new Level(defaultBoardPainter,defaultGame);
-	}
+    /**
+     * Getter for the GameState
+     * 
+     * @return
+     */
+    public GameState getGameState() {
+        return initialGame;
+    }
+
+    private static Level defaultLevel() {
+
+        List<List<Block>> defaultInnerBlocks = new ArrayList<>();
+        defaultInnerBlocks.add(Arrays.asList(Block.FREE, Block.FREE, Block.FREE,
+                Block.FREE, Block.FREE, Block.DESTRUCTIBLE_WALL, Block.FREE));
+        defaultInnerBlocks.add(Arrays.asList(Block.FREE,
+                Block.INDESTRUCTIBLE_WALL, Block.DESTRUCTIBLE_WALL,
+                Block.INDESTRUCTIBLE_WALL, Block.DESTRUCTIBLE_WALL,
+                Block.INDESTRUCTIBLE_WALL, Block.DESTRUCTIBLE_WALL));
+        defaultInnerBlocks.add(Arrays.asList(Block.FREE,
+                Block.DESTRUCTIBLE_WALL, Block.FREE, Block.FREE, Block.FREE,
+                Block.DESTRUCTIBLE_WALL, Block.FREE));
+        defaultInnerBlocks.add(Arrays.asList(Block.DESTRUCTIBLE_WALL,
+                Block.INDESTRUCTIBLE_WALL, Block.FREE,
+                Block.INDESTRUCTIBLE_WALL, Block.INDESTRUCTIBLE_WALL,
+                Block.INDESTRUCTIBLE_WALL, Block.INDESTRUCTIBLE_WALL));
+        defaultInnerBlocks.add(Arrays.asList(Block.FREE,
+                Block.DESTRUCTIBLE_WALL, Block.FREE, Block.DESTRUCTIBLE_WALL,
+                Block.FREE, Block.FREE, Block.FREE));
+        defaultInnerBlocks.add(Arrays.asList(Block.DESTRUCTIBLE_WALL,
+                Block.INDESTRUCTIBLE_WALL, Block.DESTRUCTIBLE_WALL,
+                Block.INDESTRUCTIBLE_WALL, Block.DESTRUCTIBLE_WALL,
+                Block.INDESTRUCTIBLE_WALL, Block.FREE));
+
+        Map<Block, BlockImage> defaultPalette = new HashMap<>();
+        defaultPalette.put(Block.FREE, BlockImage.IRON_FLOOR);
+        defaultPalette.put(Block.INDESTRUCTIBLE_WALL, BlockImage.DARK_BLOCK);
+        defaultPalette.put(Block.DESTRUCTIBLE_WALL, BlockImage.EXTRA);
+        defaultPalette.put(Block.CRUMBLING_WALL, BlockImage.EXTRA_O);
+        defaultPalette.put(Block.BONUS_BOMB, BlockImage.BONUS_BOMB);
+        defaultPalette.put(Block.BONUS_RANGE, BlockImage.BONUS_RANGE);
+
+        Board defaultBoard = Board.ofQuadrantNWBlocksWalled(defaultInnerBlocks);
+
+        List<Player> defaultPlayers = new ArrayList<>();
+        defaultPlayers
+                .add(new Player(PlayerID.PLAYER_1, 3, new Cell(1, 1), 2, 3));
+        defaultPlayers
+                .add(new Player(PlayerID.PLAYER_2, 3, new Cell(13, 1), 2, 3));
+        defaultPlayers
+                .add(new Player(PlayerID.PLAYER_3, 3, new Cell(13, 11), 2, 3));
+        defaultPlayers
+                .add(new Player(PlayerID.PLAYER_4, 3, new Cell(1, 11), 2, 3));
+
+        BoardPainter defaultBoardPainter = new BoardPainter(defaultPalette,
+                BlockImage.IRON_FLOOR_S);
+        GameState defaultGame = new GameState(defaultBoard, defaultPlayers);
+
+        return new Level(defaultBoardPainter, defaultGame);
+    }
 }
