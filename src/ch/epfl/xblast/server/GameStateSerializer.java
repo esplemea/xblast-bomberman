5ec776e35702a3065654b7e877ee90522a1d2688
Clara Di Marco <clara.dimarco@epfl.ch>
2016-05-10 16:19:42 +0200

    Correct the error in the serialization

--- src/ch/epfl/xblast/server/GameStateSerializer.java
+++ src/ch/epfl/xblast/server/GameStateSerializer.java
@@ -16,42 +16,55 @@ import ch.epfl.xblast.RunLengthEncoder;
  */
 
 public final class GameStateSerializer {
-	/**
-	 * This class can't be initialized
-	 */
-	private GameStateSerializer() {
-	};
+    /**
+     * This class can't be initialized
+     */
+    private GameStateSerializer() {
+    };
 
-	/**
-	 * serialize the whole game, in this order : blocks, bombs/blasts, players,
-	 * ticks.
-	 * 
-	 * @param painter
-	 * @param game
-	 * @return the encoded GameState
-	 */
-	public static List<Byte> serialize(BoardPainter painter, GameState game) {
-		List<Byte> output = new ArrayList<>();
-		for (Cell c : Cell.SPIRAL_ORDER) {
-			output.add(painter.byteForCell(game.board(), c));
-		}
-		Set<Cell> blastedCells = game.blastedCells();
-		for (Cell c : Cell.ROW_MAJOR_ORDER) {
-			if (blastedCells.contains(c)) {
-				output.add(ExplosionPainter.byteForBlast(blastedCells.contains(c.neighbor(Direction.N)),
-						blastedCells.contains(c.neighbor(Direction.E)), blastedCells.contains(c.neighbor(Direction.S)),
-						blastedCells.contains(c.neighbor(Direction.W))));
-			} else if (game.bombedCells().containsKey(c)) {
-				output.add(ExplosionPainter.byteForBomb(game.bombedCells().get(c)));
-			} else {
-				output.add(ExplosionPainter.BYTE_FOR_EMPTY);
-			}
-		}
-		output = RunLengthEncoder.encode(output);
-		for (Player player : game.players()) {
-			output.add(PlayerPainter.byteForPlayer(game.ticks(), player));
-		}
-		output.add((byte) (Math.ceil(game.remainingTime()/ 2)));
-		return output;
-	}
+    /**
+     * serialize the whole game, in this order : blocks, bombs/blasts, players,
+     * ticks.
+     * 
+     * @param painter
+     * @param game
+     * @return the encoded GameState
+     */
+    public static List<Byte> serialize(BoardPainter painter, GameState game) {
+        List<Byte> output = new ArrayList<>();
+        List<Byte> explosionOutput = new ArrayList<>();
+        for (Cell c : Cell.SPIRAL_ORDER) {
+            output.add(painter.byteForCell(game.board(), c));
+        }
+        output = RunLengthEncoder.encode(output);
+        output.add(0, (byte) output.size());
+
+        Set<Cell> blastedCells = game.blastedCells();
+        for (Cell c : Cell.ROW_MAJOR_ORDER) {
+            if (blastedCells.contains(c)) {
+                explosionOutput.add(ExplosionPainter.byteForBlast(
+                        blastedCells.contains(c.neighbor(Direction.N)),
+                        blastedCells.contains(c.neighbor(Direction.E)),
+                        blastedCells.contains(c.neighbor(Direction.S)),
+                        blastedCells.contains(c.neighbor(Direction.W))));
+            } else if (game.bombedCells().containsKey(c)) {
+                explosionOutput.add(ExplosionPainter
+                        .byteForBomb(game.bombedCells().get(c)));
+            } else {
+                explosionOutput.add(ExplosionPainter.BYTE_FOR_EMPTY);
+            }
+        }
+        explosionOutput = RunLengthEncoder.encode(explosionOutput);
+        explosionOutput.add(0, (byte) explosionOutput.size());
+
+        output.addAll(explosionOutput);
+
+        for (Player player : game.players()) {
+            output.add(PlayerPainter.byteForPlayer(game.ticks(), player));
+        }
+
+        output.add((byte) (Math.ceil(game.remainingTime() / 2)));
+
+        return output;
+    }
 }
