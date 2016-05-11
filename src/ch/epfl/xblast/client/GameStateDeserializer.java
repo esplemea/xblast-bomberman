689045ffb65db68b4ea8b6b2beecaecfe4436769
Clara Di Marco <clara.dimarco@epfl.ch>
2016-05-11 14:35:24 +0200

    Finish Deserializer

--- src/ch/epfl/xblast/client/GameStateDeserializer.java
+++ src/ch/epfl/xblast/client/GameStateDeserializer.java
@@ -3,45 +3,38 @@ package ch.epfl.xblast.client;
 
 import java.awt.Image;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import ch.epfl.xblast.Cell;
 import ch.epfl.xblast.PlayerID;
 import ch.epfl.xblast.RunLengthEncoder;
 import ch.epfl.xblast.SubCell;
 import ch.epfl.xblast.client.GameState.Player;
+import ch.epfl.xblast.client.ImageCollection;
 
 public final class GameStateDeserializer {
-    
-    /*TODO En fait serialize est censé retourner d'abord la taille, il faut corriger.. (Enfin je crois)
-    *La version compressée du plateau et des explosions est sérialisée comme suit :
-    *     un octet non signé, qui donne la taille, en octets, de la séquence compressés qui suit,
-    *     tous les octets signés composant la séquence compressée.
-    */
-    private final static int BYTES_FOR_PLAYER = 16;
-    private final static int BYTES_FOR_TIMER = 1;
-    private final static int NOT_CODED_BYTES = BYTES_FOR_PLAYER
-            + BYTES_FOR_TIMER;
 
-    /**
+    /** 
      * This object can't be initiate
      */
     private GameStateDeserializer() {
     }
 
     public static GameState deserializeGameState(List<Byte> bytes) {
-        List<Byte> decodedBytes = RunLengthEncoder
-                .decode(bytes.subList(0, bytes.size() - NOT_CODED_BYTES));
-        return new GameState(
-                desearializePlayer(bytes.subList(decodedBytes.size(),
-                        decodedBytes.size() + BYTES_FOR_PLAYER)),
-                deserializeBlocks(decodedBytes.subList(0, Cell.COUNT)),
-                deserializeExplosions(
-                        decodedBytes.subList(Cell.COUNT, Cell.COUNT * 2)),
-                deserializeScore(decodedBytes.subList(Cell.COUNT * 2,
-                        decodedBytes.size())),
-                deserializeTime(bytes.subList(bytes.size() - BYTES_FOR_TIMER,
-                        bytes.size())));
+        List<Byte> blocksBytes = new ArrayList<>(
+                bytes.subList(1, Byte.toUnsignedInt(bytes.get(0))));
+        bytes.removeAll(bytes.subList(0, Byte.toUnsignedInt(bytes.get(0))));
+        List<Byte> explosionsBytes = new ArrayList<>(
+                bytes.subList(1, Byte.toUnsignedInt(bytes.get(0))));
+        bytes.removeAll(bytes.subList(0, Byte.toUnsignedInt(bytes.get(0))));
+        blocksBytes = RunLengthEncoder.decode(blocksBytes);
+        explosionsBytes = RunLengthEncoder.decode(explosionsBytes);
+        List<Byte> playerBytes = bytes.subList(0, bytes.size()-1);
+        return new GameState(desearializePlayer(playerBytes),
+                deserializeBlocks(blocksBytes),
+                deserializeExplosions(explosionsBytes),
+                deserializeScore(playerBytes),
+                deserializeTime(bytes.get(bytes.size() - 1)));
     }
 
     private static List<Image> deserializeBlocks(List<Byte> bytes) {
@@ -65,9 +58,28 @@ public final class GameStateDeserializer {
     private static List<Image> deserializeScore(List<Byte> bytes) {
         List<Image> output = new ArrayList<>();
         ImageCollection images = new ImageCollection("score");
-        for (Byte mbyte : bytes) {
-            output.add(images.image(mbyte));
-        }
+        
+        output.addAll(imagesForOnePlayer(bytes.get(0),0));
+        output.addAll(imagesForOnePlayer(bytes.get(4),1));
+        output.addAll(Collections.nCopies(8, images.image(12)));
+        output.addAll(imagesForOnePlayer(bytes.get(8),2));
+        output.addAll(imagesForOnePlayer(bytes.get(12),3));
+        
+        return output;
+    }
+    
+    private static List<Image> imagesForOnePlayer(byte state, int ordinal) {
+        List<Image> output = new ArrayList<>();
+        ImageCollection images = new ImageCollection("score");
+        
+        if(!(Byte.toUnsignedInt(state)>0)) 
+            output.add(images.image(ordinal*2));
+        else
+            output.add(images.image((ordinal*2)+1));
+        
+        output.add(images.image(10));
+        output.add(images.image(11));
+        
         return output;
     }
 
@@ -75,23 +87,21 @@ public final class GameStateDeserializer {
         List<Player> output = new ArrayList<>();
         ImageCollection images = new ImageCollection("player");
         for (int i = 0; i < bytes.size() / 4; ++i) {
-            output.add(new Player(PlayerID.values()[i], bytes.get(4*i),
-                    new SubCell(bytes.get(4*i + 1), bytes.get(4*i + 2)),
-                    images.image(bytes.get(4*i + 3))));
+            output.add(new Player(PlayerID.values()[i], bytes.get(4 * i),
+                    new SubCell(bytes.get(4 * i + 1), bytes.get(4 * i + 2)),
+                    images.image(bytes.get(4 * i + 3))));
         }
         return output;
     }
 
-    private static List<Image> deserializeTime(List<Byte> bytes) {
+    private static List<Image> deserializeTime(Byte mbyte) {
         List<Image> output = new ArrayList<>();
         ImageCollection images = new ImageCollection("score");
-        for (Byte mbyte : bytes) {
-            for (int i = 0; i < 60; ++i) {
-                if (i < mbyte)
-                    output.add(images.image(21));
-                else
-                    output.add(images.image(20));
-            }
+        for (int i = 0; i < 60; ++i) {
+            if (i < mbyte)
+                output.add(images.image(21));
+            else
+                output.add(images.image(20));
         }
         return output;
     }
